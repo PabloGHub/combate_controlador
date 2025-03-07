21a4ea63e104fa5be8cf2e30cb3ef34dbdba0a13
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-03-07 04:53:44 +0100

    Creado proyecto Spring boot

---
+++ src/main/java/launcher/combate_controlador/CombateControladorApplication.java
@@ -0,0 +1,13 @@
+package launcher.combate_controlador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombateControladorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombateControladorApplication.class, args);
+	}
+
+}
